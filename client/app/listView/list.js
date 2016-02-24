cd3d4a30f8f7b3b516497dc11891b19bc8c1f3ad
Monica Grandy <monica.grandy@gmail.com>
2016-02-23 18:39:31 -0800

    [fix] geolocation client and server side

--- client/app/listView/list.js
+++ client/app/listView/list.js
@@ -3,53 +3,22 @@ myApp
 .controller('listCtrl', function(distance, Data, $scope, $http, $stateParams, $state) {
    $scope.data = [];
    $scope.userLocation = {};
-   $scope.geolocation = function() {
-      if (!navigator.geolocation) {
-         console.log("Your browser does not support this website.");
-         $state.go('errorView');
-      } else {
-         navigator.geolocation.getCurrentPosition(showPosition, locErr);
-         function showPosition(position) {
-            $scope.userLocation = {
-               lat: position.coords.latitude,
-               long: position.coords.longitude
-            }
-         }
-
-         function locErr (err) {
-            $state.go('errorView');
-            switch(err.code) {
-               case err.PERMISSION_DENIED:
-                  console.log("User denied the request for Geolocation.");
-                  break;
-               case err.POSITION_UNAVAILABLE:
-                  console.log("Location information is unavailable.");
-                  break;
-               case err.UNKNOWN_ERROR:
-                  console.log("An unknown error occurred.");
-                  break;
-            }
-         }
-      }
-   }
-
    $scope.transferEvent = function(obj) {
       Data.clickedItem = obj;
    }
 
    $scope.restInfo = function () {
       //Fetch data for that location
-      Data.getData($scope.userLocation)
-      .then(function(fetchedData) {
-         $scope.data = fetchedData;
-
-         for(var restaurant in $scope.data) {
-            destination = restaurant.restaurant.location;
-            restaurant.restaurant.dist = distance.calc($scope.userLocation, destination)
-         }
+      navigator.geolocation.getCurrentPosition(function(position){
+         $scope.userLocation = {
+               lat: position.coords.latitude,
+               long: position.coords.longitude
+            }
+         Data.getData($scope.userLocation)
+            .then(function(fetchedData) {
+            $scope.data = fetchedData;
+         })
       })
    }
-
-   $scope.geolocation();
    $scope.restInfo();
 });
--- server/restaurant/restController.js
+++ server/restaurant/restController.js
@@ -1,16 +1,16 @@
-var cors = require('cors');
-var Q = require('q');
 var Restaurant = require('./restModel.js');
 var PlaceSearch = require('google-locations');
 var config = require('../config.js');
 var _ = require('underscore');
 
-exports.getRestaurants = function(req, res, next){
+exports.getRestaurants = function(req, res){
   console.log(req.body);
+  var lat = req.body.lat;
+  var lng = req.body.long;
   var results = [];
   //console.log("+++line 10 - getRestaurants called")
   var locations = new PlaceSearch(config.placesKey);
-  locations.search({keyword: 'restaurant', location: [34.0192676, -118.4965371], radius: 1609.34}, function(err, response){
+  locations.search({keyword: 'restaurant', location: [lat, lng], radius: 1609.34}, function(err, response){
       if(err){
         throw err;
       }
@@ -54,81 +54,8 @@ exports.getRestaurants = function(req, res, next){
     }) 
 },  
 
-
-//helper functions for http requests
-// exports.getRestaurants = function(req, res, next){
-//   var closestRestaurants = [];
-//   var locations = new PlaceSearch(config.placesKey);
-//     locations.search({keyword: 'restaurant', location: [34.0192676, -118.4965371], radius: 1609.34}, function(err, response){
-//       if(err){
-//         throw err;
-//       }
-//       for(var i = 0; i < response.results.length-1; i++){
-//       Restaurant.findOne({ place_id: response.results[i].place_id }, function(err, newModel){
-//         if(err){
-//           console.log("+++line 21 error")
-//           var restaurant = new Restaurant({
-//             wait: "blue",
-//             geometry: {location: {lat: response.results[i].geometry.location.lat, lng: response.results[i].geometry.location.lng}},
-//             id: response.results[i].id,
-//             name: response.results[i].name,
-//             place_id: response.results[i].place_id,
-//             price_level: response.results[i].price_level,
-//             rating: response.results[i].rating,
-//             types : response.results[i].types[0],
-//             vicinity: response.results[i].vicinity
-//           })
-//           restaurant.save(function(err){
-//             if(err){
-//               console.log("not saved");
-//               throw err;
-//             }
-//             else {
-//               console.log("+++ line 38 restaurant: ", restaurant)
-//               closestRestaurants.push(restaurant)
-//             }
-
-//           })
-//         }
-//         else {
-//           console.log("+++ line 44 model: ", newModel)
-//           closestRestaurants.push(newModel)  
-//         }
-//       }) 
-//     }
-//     res.json(closestRestaurants);
-//     console.log("+++ line 49 closestRestaurants: ", closestRestaurants) 
-//   })     
-// },    
-
-// exports.fetchRestaurants = function(req, resp, next){
-//   console.log("fetch is running")
-//   var allItems = Q.nbind(Restaurant.find, Restaurant)
-//   //get all the restaurant listings
-
-//   allItems({}).then(function(restaurants){
-//     //respond with a json
-//     resp.json(restaurants);
-//   }).fail(function(error){
-//     next(error);
-//   });
-// },
-
-exports.fetchRestaurants = function(req, resp, next){
-  //query db to get all our restaurants
-  // console.log("fetch is running")
-  var allItems = Q.nbind(Restaurant.find, Restaurant)
-  //get all the restaurant listings
-  allItems({}).then(function(restaurants){
-    //respond with a json
-    resp.json(restaurants);
-  }).fail(function(error){
-    next(error);
-  });
-},
-
 //post request to update a wait time
-exports.updateWait = function(req, resp, next){
+exports.updateWait = function(req, res){
   // console.log("request obj: ", req);
     console.log("request obj: ", req.body);
   //query for the obj id
@@ -142,6 +69,6 @@ exports.updateWait = function(req, resp, next){
       if (err) {
         throw err;
       }
-    resp.json(restaurant);
+    res.json(restaurant);
     })
   }
