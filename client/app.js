16c2d603fd2db4680271f69e8d8b1010621fffeb
Cameron Roark <cam@cameronroark.com>
2016-02-23 20:41:46 -0800

    [ADD] Distance, [Refactor] listView Promises and UI Router

--- client/app.js
+++ client/app.js
@@ -4,21 +4,18 @@ var myApp = angular.module('lunchline', ['ui.router', 'ui.bootstrap', 'mobile-an
    $stateProvider
    .state('home', {
       url: '/home',
-      templateUrl: './app/listView/list.html',
-      controller: 'listCtrl'
+      templateUrl: './app/listView/list.html'
    })
    .state('restView', {
       url: '/restView',
       templateUrl: './app/restView/rest.html',
       controller: 'restCtrl'
    })
-   .state('listView', {
-      url: '/listView',
-      templateUrl: './app/listView/list.html',
-      controller: 'listCtrl'
-   })
    .state('errorView', {
       url: '/error',
       templateUrl: './err.html'
    })
+})
+.config(function($urlRouterProvider) {
+   $urlRouterProvider.otherwise('/home');
 });
--- client/app/listView/list.js
+++ client/app/listView/list.js
@@ -1,6 +1,7 @@
 //Retrieve information from the server and serve it in HTML.
 myApp
 .controller('listCtrl', function(distance, Data, $scope, $http, $stateParams, $state) {
+   console.log('called');
    $scope.data = [];
    $scope.userLocation = {};
    $scope.transferEvent = function(obj) {
@@ -13,12 +14,20 @@ myApp
          $scope.userLocation = {
                lat: position.coords.latitude,
                long: position.coords.longitude
-            }
-         Data.getData($scope.userLocation)
-            .then(function(fetchedData) {
+         };
+         Data.getData($scope.userLocation, function (fetchedData) {
+            //Make a distance property for each restaurant
+            for(var i = 0; i < fetchedData.length; i++) {
+               var destination = {
+                  lat: fetchedData[i].restaurant.geometry.location.lat,
+                  long: fetchedData[i].restaurant.geometry.location.lng
+               };
+            fetchedData[i].restaurant.dist = distance.calc($scope.userLocation, destination);
             $scope.data = fetchedData;
-         })
-      })
+            console.log('FetchedData: ', fetchedData);
+            }
+         });
+      });
    }
    $scope.restInfo();
 });
